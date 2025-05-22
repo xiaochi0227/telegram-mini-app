2fd1b80c0b78b30e4de61ad693cb498fbf7473c2
xiaochi <601862548@qq.com>
2025-05-22 14:58:18 +0800

    feat: xiugai

--- src/store/index.ts
+++ src/store/index.ts
@@ -63,15 +63,12 @@ export const useAppStore = defineStore('app', {
 
       const webApp = window.Telegram?.WebApp;
 
-      if (!webApp) return
-
       // 检查用户信息
       if (webApp.initDataUnsafe.user) {
         this.setUser(webApp.initDataUnsafe.user);
       } else {
         Notify({ type: 'danger', message: '用户未授权，请通过 Telegram 客户端重新打开 WebApp。'})
       }
-
       // 校验
       const res = await authApi.tgUserCheck(webApp.initData)
 
